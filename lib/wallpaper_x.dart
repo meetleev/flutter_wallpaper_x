53f610deb2804f152433e95c1107a7d0c62af89b
meetleev <meetleev@gmail.com>
2024-11-05 15:19:04 +0800

    added public API has dartdoc comments

--- lib/wallpaper_x.dart
+++ lib/wallpaper_x.dart
@@ -3,9 +3,14 @@ import 'src/pigeon/messages.g.dart';
 class WallpaperX {
   final WallpaperXInterface _wallpaperXInterface = WallpaperXInterface();
 
+  /// instance
   factory WallpaperX() => instance;
+
+  /// instance
   static WallpaperX get instance => WallpaperX._();
   WallpaperX._();
+
+  /// setLiveWallpaper
   Future<bool> setLiveWallpaper({required String filePath}) async {
     return _wallpaperXInterface.setLiveWallpaper(filePath);
   }
